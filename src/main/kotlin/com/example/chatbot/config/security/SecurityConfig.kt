e9dedf3f2c6e51add4a63f51d145d4c6df600b95
season <sionwer4@naver.com>
2024-12-20 21:15:17 +0900

    feat: Add EnableMethodSecurity to security config

--- src/main/kotlin/com/example/chatbot/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/chatbot/config/security/SecurityConfig.kt
@@ -2,11 +2,13 @@ package com.example.chatbot.config.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
+@EnableMethodSecurity
 class SecurityConfig (
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
     private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint,

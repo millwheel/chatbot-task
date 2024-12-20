32ebe7ffe0aecdc3a6849b62c4be67c7f0b3e13c
season <sionwer4@naver.com>
2024-12-20 23:11:53 +0900

    feat: change the format of exception response

--- src/main/kotlin/com/example/chatbot/exception/dto/ErrorResult.kt
+++ src/main/kotlin/com/example/chatbot/exception/dto/ErrorResult.kt
@@ -1,6 +1,17 @@
 package com.example.chatbot.exception.dto
 
-data class ErrorResult (
+import org.springframework.http.HttpStatus
+
+data class ErrorResult(
     val code: String,
     val message: String
-)
+) {
+    companion object {
+        fun of(status: HttpStatus, message: String): ErrorResult {
+            return ErrorResult(
+                code = status.reasonPhrase,
+                message = message
+            )
+        }
+    }
+}
--- src/main/kotlin/com/example/chatbot/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/chatbot/exception/handler/GlobalExceptionHandler.kt
@@ -2,26 +2,32 @@ package com.example.chatbot.exception.handler
 
 import com.example.chatbot.exception.custom.DataNotFoundException
 import com.example.chatbot.exception.custom.InvalidEmailOrPasswordException
+import com.example.chatbot.exception.dto.ErrorResult
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 
+
+
 @ControllerAdvice
 class GlobalExceptionHandler {
 
+    private val logger = KotlinLogging.logger {}
 
     @ExceptionHandler(InvalidEmailOrPasswordException::class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    fun handleInvalidEmailOrPasswordException(ex: InvalidEmailOrPasswordException): ResponseEntity<String> {
-        return ResponseEntity(ex.message, HttpStatus.UNAUTHORIZED)
+    fun handleInvalidEmailOrPasswordException(ex: InvalidEmailOrPasswordException): ErrorResult {
+        logger.error{ ex.message }
+        return ErrorResult.of(HttpStatus.UNAUTHORIZED, ex.message!!)
     }
 
     @ExceptionHandler(DataNotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleDataNotFoundException(ex:DataNotFoundException): ResponseEntity<String> {
-        return ResponseEntity(ex.message, HttpStatus.NOT_FOUND)
+    fun handleDataNotFoundException(ex:DataNotFoundException): ErrorResult {
+        logger.error{ ex.message }
+        return ErrorResult.of(HttpStatus.NOT_FOUND, ex.message!!)
     }
 
 }

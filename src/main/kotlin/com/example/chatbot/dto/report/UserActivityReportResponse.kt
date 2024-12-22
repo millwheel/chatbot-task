fc03bdd9e39758fa74440fdfbc2c43ff6b678538
season <sionwer4@naver.com>
2024-12-22 14:52:33 +0900

    feat: Change the response property of user activity report response

--- src/main/kotlin/com/example/chatbot/dto/report/UserActivityReportResponse.kt
+++ src/main/kotlin/com/example/chatbot/dto/report/UserActivityReportResponse.kt
@@ -3,17 +3,17 @@ package com.example.chatbot.dto.report
 import java.time.LocalDate
 
 data class UserActivityReportResponse (
-    val signUp: Int,
-    val login: Int,
-    val chatCreation: Int,
+    val signUpCount: Int,
+    val loginCount: Int,
+    val chatCreationCount: Int,
     val date : LocalDate
 ) {
     companion object {
         fun of (signUpCount: Int, loginCount: Int, chatCreationCount:Int, localDate: LocalDate): UserActivityReportResponse {
             return UserActivityReportResponse(
-                signUp = signUpCount,
-                login = loginCount,
-                chatCreation = chatCreationCount,
+                signUpCount = signUpCount,
+                loginCount = loginCount,
+                chatCreationCount = chatCreationCount,
                 date = localDate
             )
         }

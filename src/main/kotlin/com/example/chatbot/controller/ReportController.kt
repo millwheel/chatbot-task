8fca01b65ac35c6d1f547c95821570c052ca71d8
season <sionwer4@naver.com>
2024-12-23 13:43:03 +0900

    feat: Add download file response formatter and implement csv download

--- src/main/kotlin/com/example/chatbot/controller/ReportController.kt
+++ src/main/kotlin/com/example/chatbot/controller/ReportController.kt
@@ -4,11 +4,14 @@ import com.example.chatbot.dto.report.ChatReportResponse
 import com.example.chatbot.dto.report.UserActivityReportResponse
 import com.example.chatbot.service.report.ChatReportService
 import com.example.chatbot.service.report.UserActivityReportService
-import jakarta.servlet.http.HttpServletResponse
+import com.example.chatbot.util.constructFileResponse
+import org.springframework.http.*
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 @RequestMapping("/reports")
 @RestController
@@ -29,18 +32,19 @@ class ReportController (
         return chatReportLogService.getChatReport("KR")
     }
 
-//    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/chats/today/csv")
-    fun getChatReportAsCsv(response: HttpServletResponse) {
-        val chatReport = chatReportLogService.getChatReport("KR")
-        response.setHeader("Content-Type", "text/csv; charset=UTF-8")
-        response.setHeader("Content-Disposition", "attachment; filename=\"chat_report_${chatReport.date}.csv\"")
-        response.writer.use { writer ->
-            writer.append("Question,Answer,User Email\n")
-            chatReport.logs.forEach {
-                writer.append("${it.question},${it.answer},${it.userEmail}\n")
+    fun getChatReportAsCsv() : ResponseEntity<ByteArray> {
+        val chatReportResponse = chatReportLogService.getChatReport("KR")
+        val filename = "chat_report_${chatReportResponse.date}.csv"
+        val csvContent = buildString {
+            append("question,answer,userEmail\n")
+            chatReportResponse.logs.forEach {
+                append("\"${it.question}\",\"${it.answer}\",\"${it.userEmail}\"\n")
             }
         }
+        val byteArray = csvContent.toByteArray(Charsets.UTF_8)
+        return constructFileResponse(byteArray, filename)
     }
 
 }

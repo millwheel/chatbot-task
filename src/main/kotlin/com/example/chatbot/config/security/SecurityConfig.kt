3e69212ba865968c156412da2b96567f5b62e5de
season <sionwer4@naver.com>
2024-12-29 17:28:31 +0900

    feat: Add rest authentication filter to filter chain

--- src/main/kotlin/com/example/chatbot/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/chatbot/config/security/SecurityConfig.kt
@@ -1,10 +1,10 @@
 package com.example.chatbot.config.security
 
-import com.example.chatbot.config.security.component.CustomAccessDeniedHandler
-import com.example.chatbot.config.security.component.CustomAuthenticationEntryPoint
-import com.example.chatbot.config.security.component.JwtAuthenticationFilter
+import com.example.chatbot.config.security.component.RestAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -12,23 +12,30 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableMethodSecurity
-class SecurityConfig (
-    private val jwtAuthenticationFilter: JwtAuthenticationFilter
-){
+class SecurityConfig {
 
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    fun securityFilterChain(http: HttpSecurity, authenticationManagerBuilder: AuthenticationManagerBuilder): SecurityFilterChain {
+        val authenticationManager = authenticationManagerBuilder.build()
+
         http
             .authorizeHttpRequests { authorization -> authorization
                 .requestMatchers( "/actuator/**", "/error/**", "/auth/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/docs/**", "/swagger-resources/**").permitAll()
                 .anyRequest().authenticated()
             }
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
-            .csrf{
+            .authenticationManager(authenticationManager)
+            .addFilterBefore(getRestAuthorizationFilter(authenticationManager), UsernamePasswordAuthenticationFilter::class.java)
+            .csrf {
                 it.disable()
             }
         return http.build()
+    }
+
+    fun getRestAuthorizationFilter(authenticationManager: AuthenticationManager): RestAuthenticationFilter {
+        val restAuthenticationFilter = RestAuthenticationFilter()
+        restAuthenticationFilter.setAuthenticationManager(authenticationManager)
+        return restAuthenticationFilter
     }
 
 }
